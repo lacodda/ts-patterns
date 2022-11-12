23e9346e478ab5e783a53436fded9314073bcec2
Kirill Lakhtachev <lahtachev@gmail.com>
2022-11-12 14:10:04 +0100

    add command

---
+++ src/behavioral/chain-of-command.ts
@@ -0,0 +1,78 @@
+// IMiddleware
+interface IMiddleware {
+  next: (mid: IMiddleware) => IMiddleware;
+  handle: (request: any) => any;
+}
+
+// AbstractMiddleware
+abstract class AbstractMiddleware implements IMiddleware {
+  private nextMiddleware?: IMiddleware;
+
+  next(mid: IMiddleware): IMiddleware {
+    this.nextMiddleware = mid;
+    return mid;
+  }
+
+  handle(request: any): any {
+    if (this.nextMiddleware) {
+      return this.nextMiddleware.handle(request);
+    }
+  }
+}
+
+// AuthMiddleware
+class AuthMiddleware extends AbstractMiddleware {
+  override handle(request: any): any {
+    console.log('AuthMiddleware');
+    if (request.userId === 1) {
+      return super.handle(request);
+    }
+    return { error: 'You are not authorized!' };
+  }
+}
+
+// ValidateMiddleware
+class ValidateMiddleware extends AbstractMiddleware {
+  override handle(request: any): any {
+    console.log('ValidateMiddleware');
+    if (request.body) {
+      return super.handle(request);
+    }
+    return { error: 'No body!' };
+  }
+}
+
+// Controller
+class Controller extends AbstractMiddleware {
+  override handle(request: any): any {
+    console.log('Controller');
+    return { success: request };
+  }
+}
+
+// Use
+const controller = new Controller();
+const validate = new ValidateMiddleware();
+const auth = new AuthMiddleware();
+
+auth.next(validate).next(controller);
+
+console.log(
+  auth.handle({
+    userId: 1,
+    body: 'I am OK!'
+  })
+);
+
+console.log(
+  auth.handle({
+    userId: 2,
+    body: 'I am OK!'
+  })
+);
+
+console.log(
+  auth.handle({
+    userId: 1
+  })
+);
